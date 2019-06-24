3da1a6033ac816add2345d2ec11e015dfa1c5b4a
Lawrence <lancewiu@tutanota.com>
2019-06-24 17:26:00 -0400

    minor change to patch type

--- +experiment/PatchType.m
+++ +experiment/PatchType.m
@@ -1,22 +1,28 @@
 classdef PatchType < double
     %PATCHTYPE Type of Patch Amplifier
     enumeration
-       NoPatch (1)
-       Axopatch200B (2)
-       Multiclamp700 (3)
+        NoPatch (1)
+        Axopatch200B (2)
+        Multiclamp700B (3)
     end
     
     methods
         function str = char(obj)
-            switch obj
-                case experiment.PatchType.NoPatch
-                    str = 'No Patch';
-                case experiment.PatchType.Axopatch200B
-                    str = 'Axopatch 200B';
-                case experiment.PatchType.Multiclamp700
-                    str = 'Multiclamp 700';
-                otherwise
-                    error('obj is not an experiment.PatchType!');
+            str = cell(size(obj));
+            for iObj=1:length(obj)
+                switch obj(iObj)
+                    case experiment.PatchType.NoPatch
+                        str{iObj} = 'No Patch';
+                    case experiment.PatchType.Axopatch200B
+                        str{iObj} = 'Axopatch 200B';
+                    case experiment.PatchType.Multiclamp700B
+                        str{iObj} = 'Multiclamp 700B';
+                    otherwise
+                        error('obj is not an experiment.PatchType!');
+                end
+            end
+            if isscalar(str)
+                str = str{1};
             end
         end
     end
